80139dae77497fd461e9ea97e8b9e37c9beb7d0e
Harriet Anderson <harriet.correspondence@gmail.com>
2025-05-15 13:03:04 +0100

    steps 1-6 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "cleaning the DB"
+
+Owner.destroy_all
+Car.destroy_all
+
+puts "creating owners"
+
+ife = Owner.create!(nickname: "Ife500")
+deji = Owner.create!(nickname: "DejiGasGas")
+claire = Owner.create!(nickname: "WonderClaire")
+ben = Owner.create!(nickname: "BenMcClaren")
+
+puts "number of owners created #{Owner.count}"
+
+puts "creating cars"
+
+Car.create!(brand: "Mercedes", model: "A-Class Hatchback", year: 2024, fuel: "Unleaded petrol", owner: deji)
+Car.create!(brand: "Peugeot", model: "308", year: 2017, fuel: "unleaded petrol", owner: ife)
+Car.create!(brand: "BMW", model: "mini-cooper", year: 2025, fuel: "Unleaded Petrol", owner: claire)
+
+puts "number of cars created #{Car.count}"
